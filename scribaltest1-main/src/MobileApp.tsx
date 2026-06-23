aee1cf8194c1c38db192771b80888eca77326679
kepz94 <kasaha01@gmail.com>
2026-06-23 04:50:13 -0600

    Update MobileApp.tsx

--- scribaltest1-main/src/MobileApp.tsx
+++ scribaltest1-main/src/MobileApp.tsx
@@ -6922,7 +6922,6 @@ export default function MobileApp() {
                   ? "covenants"
                   : "outline"
               }
-              viewLocked={(cr?.view ?? cs?.view) === "covenants"}
               relSavedRoles={scopedRoles[cScope]}
               onRelRoles={(r) => setScopedRoles(cScope, r)}
               marks={cMarks}
