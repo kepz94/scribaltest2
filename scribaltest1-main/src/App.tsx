ca21b29d9744c5bb093a8c0a0ac61eec28cdd481
kepz94 <kasaha01@gmail.com>
2026-07-01 09:39:31 -0600

    Update App.tsx

--- scribaltest1-main/src/App.tsx
+++ scribaltest1-main/src/App.tsx
@@ -10215,7 +10215,9 @@ export default function App() {
         <StudyTablesDesktop
           onClose={() => setMode("read")}
           headerOffset={headerH}
-          marks={marks}
+          allMarks={allMarks}
+          getBook={getBook}
+          books={books}
         />
       )}
 
